0e9da07d08bfb28b2e2713b9c8575080c763cd4e
NllKara <lupaakun2202@gmail.com>
2026-08-27 11:57:40 +0700

    Switch production build to data driven NIGHT terminal

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from'react';
 import{createRoot}from'react-dom/client';
-import App from'./App';
+import ProductionApp from'./ProductionApp';
 import'./style.css';
 import'./institution.css';
 import'./equity.css';
+import'./production.css';
 
-createRoot(document.getElementById('root')).render(<App/>);
+createRoot(document.getElementById('root')).render(<ProductionApp/>);
