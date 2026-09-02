1c67fd85643423e7bf5bd38d9716abfcace3321d
NllKara <lupaakun2202@gmail.com>
2026-09-02 21:47:18 +0700

    Activate full FX universe and News Prediction tab

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from'react';
 import{createRoot}from'react-dom/client';
 import ProductionApp from'./ProductionApp';
+import TerminalEnhancements from'./TerminalEnhancements';
 import'./style.css';
 import'./institution.css';
 import'./equity.css';
 import'./production.css';
 import'./ndr-layout.css';
+import'./news-prediction.css';
 
-createRoot(document.getElementById('root')).render(<ProductionApp/>);
+createRoot(document.getElementById('root')).render(<><ProductionApp/><TerminalEnhancements/></>);
